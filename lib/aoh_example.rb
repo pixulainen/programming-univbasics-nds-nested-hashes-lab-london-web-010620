8df290cfb21fcf8171df3e76e204c78138b6f630
George Apetrei <george.apetrei@gmail.com>
2019-12-20 13:46:08 +0000

    Done.

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -1,5 +1,5 @@
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
-
+require "pry"
 DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
 JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
 PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
@@ -7,19 +7,25 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
+  arr = DON_G,JOELLE_VD,PAT_M,KATE_G,BRUCE_G
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
 end
 
 def literal_aoh
+hash=DON_G,JOELLE_VD,PAT_M,KATE_G,BRUCE_G
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
 
 def aoh_lookup(aoh, row, key)
-end
+aoh[row][key]
 
+end
 def aoh_update(aoh, row, key, new_value)
+  aoh[row][key] = new_value
+  aoh
+
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
